0e9bbfb325bb9965782c9b1df6850e1fb8daaa3e
do.ockhyun <do.ockhyun@joins.com>
2021-04-29 16:02:18 +0900

    lombok

--- src/main/java/com/misolab/signle/SignleApplication.java
+++ src/main/java/com/misolab/signle/SignleApplication.java
@@ -1,9 +1,11 @@
 package com.misolab.signle;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class SignleApplication implements CommandLineRunner {
 
@@ -13,6 +15,6 @@ public class SignleApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("run");
+		log.info("run {}", args);
 	}
 }
