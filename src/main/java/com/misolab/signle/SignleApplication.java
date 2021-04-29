c62b13998079a17c7b38710a6bc9b7802e6e3677
do.ockhyun <do.ockhyun@joins.com>
2021-04-29 16:05:45 +0900

    actuator

--- src/main/java/com/misolab/signle/SignleApplication.java
+++ src/main/java/com/misolab/signle/SignleApplication.java
@@ -1,20 +1,15 @@
 package com.misolab.signle;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
+/**
+ * @author ock
+ */
 @SpringBootApplication
-public class SignleApplication implements CommandLineRunner {
+public class SignleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SignleApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		log.info("run {}", args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SignleApplication.class, args);
+    }
 }
