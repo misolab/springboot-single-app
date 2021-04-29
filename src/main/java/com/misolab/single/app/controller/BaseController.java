1ab53c1fe4d04078de903554caa58f3e007121cc
do.ockhyun <do.ockhyun@joins.com>
2021-04-29 17:37:11 +0900

    add app package

---
+++ src/main/java/com/misolab/single/app/controller/BaseController.java
@@ -0,0 +1,16 @@
+package com.misolab.single.app.controller;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BaseController {
+
+    @ModelAttribute("common")
+    public Map<String, String> common() {
+        Map<String, String> model = new HashMap<>();
+        
+        return model;
+    }
+}
