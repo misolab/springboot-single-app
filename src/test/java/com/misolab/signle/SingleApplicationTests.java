8113c3e5db148de81b4ddb9ed22833fce9c6836a
do.ockhyun <do.ockhyun@joins.com>
2021-04-30 09:11:32 +0900

    테스트 코드 수정

--- src/test/java/com/misolab/signle/SingleApplicationTests.java
+++ src/test/java/com/misolab/signle/SingleApplicationTests.java
@@ -1,9 +1,10 @@
 package com.misolab.signle;
 
+import com.misolab.single.SingleApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SingleApplication.class)
 class SingleApplicationTests {
 
 	@Test
