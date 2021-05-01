e838ebffa585fc3cc3193e240c9158e5815b4e27
do.ockhyun <do.ockhyun@joins.com>
2021-05-01 16:26:58 +0900

    archetype 변경 코드 반영

--- src/main/java/com/misolab/single/app/controller/BaseController.java
+++ src/main/java/com/misolab/single/app/controller/BaseController.java
@@ -1,16 +1,12 @@
 package com.misolab.single.app.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class BaseController {
 
     @ModelAttribute("common")
-    public Map<String, String> common() {
-        Map<String, String> model = new HashMap<>();
+    public void common(Model model) {
 
-        return model;
     }
 }
